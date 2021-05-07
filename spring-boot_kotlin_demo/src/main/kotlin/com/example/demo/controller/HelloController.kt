ea0b75a45b0c8e9f616271e18397273d909fdaa6
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-05-07 09:04:51 +0900

    spring ハローワールド/spring DB プロジェクト開始

--- spring-boot_kotlin_demo/src/main/kotlin/com/example/demo/controller/HelloController.kt
+++ spring-boot_kotlin_demo/src/main/kotlin/com/example/demo/controller/HelloController.kt
@@ -2,22 +2,23 @@ package com.example.demo.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 // @RestController：API機能のみ、view遷移せずメソッドの戻り値がそのままHTTPリクエストのレスポンスとなる
-@RestController
+@Controller
 class HelloController {
     // @RequestMapping()：リクエストを受け付けるパスを指定する
-    @RequestMapping("/hello")
-    fun index(): String {
-        return "ハローワールド！！！！"
-    }
-
-//    @GetMapping("/hello")
-//    fun index(model: Model): String {
-//        model.addAttribute("Greeting", "ハローワールド！！")
-//        // テンプレート（HTMLファイル名）を返す
-//        return "index"
+//    @RequestMapping("/hello")
+//    fun index(): String {
+//        return "ハローワールド！！！！"
 //    }
+
+    @GetMapping("/hello")
+    fun index(model: Model): String {
+        // Greeting という名前で ハローワールド！！ という文字列をviewに渡している
+        model.addAttribute("Greeting", "ハローワールド！！")
+        // テンプレート（HTMLファイル名）を返す
+        return "hello"
+    }
 }
