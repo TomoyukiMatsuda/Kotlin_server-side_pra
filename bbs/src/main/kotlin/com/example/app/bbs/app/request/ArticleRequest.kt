6ade79f0d683958fedbd57c18be588aa255c89f0
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-05-10 09:17:09 +0900

    バリデーション利用コメント追加

--- bbs/src/main/kotlin/com/example/app/bbs/app/request/ArticleRequest.kt
+++ bbs/src/main/kotlin/com/example/app/bbs/app/request/ArticleRequest.kt
@@ -3,6 +3,12 @@ package com.example.app.bbs.app.request
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
+// @NotBlank などのバリデーションを利用するためには、build.gradle に下記記述を追加する必要あり
+//dependencies {
+//        implementation("org.springframework.boot:spring-boot-starter-validation")
+//}
+
+
 data class ArticleRequest(
         var id: Int = 0,
         @field:NotBlank
