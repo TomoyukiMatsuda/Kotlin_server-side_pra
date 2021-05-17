b8265e4f75e5432d5c23b3ba71f495be6e1fc983
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-05-17 09:37:06 +0900

    コンストラクタインジェクションを学習

--- jissenkaihatu/src/main/kotlin/com/example/jissenkaihatu/HelloController.kt
+++ jissenkaihatu/src/main/kotlin/com/example/jissenkaihatu/HelloController.kt
@@ -1,5 +1,7 @@
 package com.example.jissenkaihatu
 
+import org.apache.coyote.Response
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.*
 
 
@@ -13,4 +15,25 @@ class HelloController {
     fun index(@RequestBody request: Request): Message {
         return Message("ハロー ${request.name}")
     }
+}
+
+
+// TODO: 「第２部 3 Spring FrameworkのDIを使用する」 からやる！！
+// コンストラクタインジェクションによりDIされて、クラス内でgreeterが利用できる
+@RestController
+class GreeterController(private val greeter: Greeter) {
+    @GetMapping("/hello/{name}")
+    fun helloByService(@PathVariable("name") name: String): Message {
+        val message = greeter.hello(name)
+        return Message(message)
+    }
+}
+
+interface Greeter {
+    fun hello(name: String): String
+}
+
+@Component // DIの対象であることを表す、DIしたオブジェクトはシングルトンになる
+class GreeterImpl : Greeter {
+    override fun hello(name: String): String = "ハロー $name"
 }
